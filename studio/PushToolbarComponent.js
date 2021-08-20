2c5cce591f896d5b87a20cc5c1a1b105d20c9bf5
Michaela DeForest <mdeforest@galileosuite.com>
2021-08-20 09:08:04 -0400

    additional components

--- studio/PushToolbarComponent.js
+++ studio/PushToolbarComponent.js
@@ -1,18 +1,47 @@
 import Studio from 'jsreport-studio'
+import { useEffect, useState } from 'react'
+import style from './VersionControlGit.scss'
 
 const PushToolbar = () => {
+  const [expandedToolbar, setExpandedToolbar] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('click', tryHide())
+    return () => {
+      window.removeEventListener('click', tryHide())
+    }
+  }, [])
+
+  const tryHide = () => {
+    setExpandedToolbar(false)
+  }
+
   const openLocalCommits = (e) => {
     e.stopPropagation()
     // Open tab
     Studio.openTab({ key: 'LocalCommitsPage', editorComponentKey: 'localCommits', title: 'Local Commits' })
   }
 
+  const openPull = async (e) => {
+    e.stopPropagation()
+    Studio.openTab({ key: 'RemotePullPage', editorComponentKey: 'remotePull', title: 'New Changes from Remote' })
+  }
+
   return <div
     title="Push all un-pushed commits"
     className="toolbar-button"
     onClick={(e) => openLocalCommits(e)}>
-    <i className="fa fa-upload"></i>
-    <span>Push Changes</span>
+    <i className="fa fa-git-alt"></i>
+    <span>Remote Git</span>
+    <span className={style.runCaret} onClick={(e) => { e.stopPropagation(); setExpandedToolbar(!expandedToolbar) }} />
+    <div className={style.runPopup} style={{ display: expandedToolbar ? 'block' : 'none' }}>
+      <div title='Push' className='toolbar-button' onClick={(e) => openLocalCommits(e)}>
+        <i className='fa fa-upload' /><span>Push Changes</span>
+      </div>
+      <div title='Pull' className='toolbar-button' onClick={(e) => openPull(e)}>
+        <i className='fa fa-download' /><span>Pull Changes</span>
+      </div>
+    </div>
   </div>
 }
 
